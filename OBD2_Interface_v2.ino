76e55e7a290f1dc3718cb4b92034d8c9c2beaef0
Kevin Guest <github@bionicbone.plus.com>
2024-09-09 13:01:18 +0100

    Add project files.

---
+++ OBD2_Interface_v2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		OBD2_Interface_v2.ino
+ Created:	Sep 2024
+ Author:	Kevin Guest AKA TheBionicBone
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
